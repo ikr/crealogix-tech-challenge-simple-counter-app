3edfe1c85b69c967ff430f170ff9be305193a69c
Ivan Krechetov <ikr@ikr.su>
2018-02-23 09:19:41 +0100

    Implement the state operations

---
+++ src/state.js
@@ -0,0 +1,31 @@
+export function initial () {
+    return {series: [0]}
+}
+
+export function reduce (db, action) {
+    if (!db) {
+        return initial()
+    }
+
+    const series = db.series.slice()
+    const currentValue = series[series.length - 1]
+
+    switch (action) {
+    case 'PLUS':
+        series.push(currentValue + 1)
+        break
+
+    case 'MINUS':
+        if (currentValue > 0) {
+            series.push(currentValue - 1)
+        }
+
+        break
+    }
+
+    return {series}
+}
+
+export function currentValue(db) {
+    return db.series[db.series.length - 1]
+}
---
+++ tests/state.spec.js
@@ -0,0 +1,38 @@
+import assert from 'assert'
+import * as state from '../src/state'
+
+describe('state', () => {
+    describe('initial', () => {
+        it('is a series array containing just one zero', () => {
+            assert.deepEqual(state.initial(), {series: [0]})
+        })
+    })
+
+    describe('reduce', () => {
+        it('returns initial on undefined', () => {
+            assert.deepEqual(state.reduce(undefined, ''), state.initial())
+        })
+
+        describe('on PLUS', () => {
+            it('appends incremented last number to the end of series', () => {
+                assert.deepEqual(state.reduce({series: [0, 1, 2]}, 'PLUS'), {series: [0, 1, 2, 3]})
+            })
+        })
+
+        describe('on MINUS', () => {
+            it('appends decremented last number to the end of series', () => {
+                assert.deepEqual(state.reduce({series: [0, 1, 2]}, 'MINUS'), {series: [0, 1, 2, 1]})
+            })
+
+            it('is identical when the last element in series is zero', () => {
+                assert.deepEqual(state.reduce({series: [0, 1, 0]}, 'MINUS'), {series: [0, 1, 0]})
+            })
+        })
+    })
+
+    describe('currentValue', () => {
+        it('returns the last element of series', () => {
+            assert.strictEqual(state.currentValue({series: [0, 1, 2, 3, 2]}), 2)
+        })
+    })
+})
